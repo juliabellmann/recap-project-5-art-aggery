1b792ae5b9de91f16cb8c734ffe9f222b749cdba
ana-nova <yu.ana@gmx.de>
2024-10-21 12:24:32 +0200

    solve issue 2

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
 import ArtPieces from "@/_components/ArtPieces";
+import Spotlight from "@/_components/Spotlight";
 import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function HomePage() {
-  const { data, error } = useSWR("https://example-apis.vercel.app/api/art", fetcher);
+  const { data, error } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
 
-  if (error) return <div>API nicht voranden</div>
-  if (!data) return <div>Daten können nicht geladen werden</div>
-  
+  if (error) return <div>API nicht voranden</div>;
+  if (!data) return <div>Daten können nicht geladen werden</div>;
+
+  const randomIndex = Math.floor(Math.random() * data.length);
+  console.log(randomIndex);
+
   return (
     <div>
-      <ArtPieces pieces={data}/>
+      <ArtPieces pieces={data} />
+      <Spotlight
+        image={data[randomIndex].imageSource}
+        artist={data[randomIndex].artist}
+      />
     </div>
   );
 }
+
+// function getRandomArtPieces(min, max) {
+//   const minPiece = Math.ceil(min);
+//   const maxPiece = Math.floor(max);
+//   return Math.floor(Math.random() * (maxPiece - minPiece) + minPiece);
+// }
+// const randomArtPieces = getRandomArtPieces(0, 12);
